99156544fed8c063acff0828559e8608ceef1b44
Abel Gaming <lcpdpolice4939@gmail.com>
2020-09-21 22:01:59 -0400

    Create Main.cs

---
+++ Input Codes/Main.cs
@@ -0,0 +1,24 @@
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+using System.Threading.Tasks;
+using System;
+
+namespace TrunkInventory.Commands
+{
+    public class CreateCommands : BaseScript
+    {
+        public CreateCommands()
+        {
+            Tick += OnTick;
+        }
+
+        private static async Task OnTick()
+        {
+            if (Game.Player.Character.IsInVehicle())
+            {
+              Screen.DisplayHelpTextThisFrame("Press ~INPUT_PICKUP~ to toggle siren"); //THIS WILL SHOW THE "E" key or controller "LB" button
+            }
+        }
+    }
+}
